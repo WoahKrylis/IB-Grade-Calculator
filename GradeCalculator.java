08f638dc4e68da03a48c173c110073f08e2d52b2
WoahKrylis <warrenaji18@gmail.com>
2026-09-06 20:31:31 +0200

    Modified GradeCalculator.java

---
+++ GradeCalculator.java
@@ -0,0 +1,9 @@
+public class GradeCalculator { // in order to make the container
+    public static void main(String[] args) { // "Engine" of the thing essentially.
+
+        System.out.println("Test."); // this is a test
+
+        // Pretty much code goes inside here since this is "inside the engine" so what gonna run
+
+    } // End of the engine
+} // end of code iirc
