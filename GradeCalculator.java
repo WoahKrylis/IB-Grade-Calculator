ffb437001d21ac3c52b101776a9183ca733cdc8b
WoahKrylis <warrenaji18@gmail.com>
2026-09-07 12:43:03 +0200

    Modified GradeCalculator.java

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,9 +1,11 @@
 public class GradeCalculator { // in order to make the container
     public static void main(String[] args) { // "Engine" of the thing essentially.
 
-        System.out.println("Test."); // this is a test
+        int grade = 85; // Apparently 85% is a 7 so we go with that
+
+
 
         // Pretty much code goes inside here since this is "inside the engine" so what gonna run
 
     } // End of the engine
-} // end of code iirc
+} // end of code
