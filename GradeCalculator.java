6a17b6f60012a2bf5f109cd83545bfc724678f47
WoahKrylis <warrenaji18@gmail.com>
2026-09-07 14:21:35 +0200

    Modified GradeCalculator.java

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,7 +1,13 @@
+import java.util.Scanner; // Scans user input
 public class GradeCalculator { // in order to make the container
-    public static void main(String[] args) { // "Engine" of the thing essentially.
+    public static void main(String[] args) throws Exception{ // "Engine" of the thing essentially.
+        System.out.println("Welcome to IB Grade Calculator");
+        Thread.sleep(2000);
+        System.out.println("Please enter your grade");
+       // int grade = 85; // Apparently 85% is a 7 so we go with that
 
-        int grade = 85; // Apparently 85% is a 7 so we go with that
+Scanner ginput = new Scanner(System.in); // yeah
+int grade = ginput.nextInt(); // all this asks makes prompts u to write the thing
 
 
 
